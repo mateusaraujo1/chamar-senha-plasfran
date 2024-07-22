8d287c10699f4ba38442aa961151be9861e1d73a
Mateus Araújo <f.mateusaraujo20@gmail.com>
2024-07-22 15:10:50 -0300

    add bootstrap5 and img file

--- index.php
+++ index.php
@@ -2,18 +2,30 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="bootstrap-5/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Contatos</title>
+
 </head>
 
 <body>
 
-    <?php
-    
-    phpinfo();
-    
-    ?>
+    <header>
+
+
+
+    </header>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 
 </body>
 
