086712a4722bc68580b99f140b72f056456b5751
Mateus Araújo <f.mateusaraujo20@gmail.com>
2024-07-22 16:57:25 -0300

    finish login page

--- index.php
+++ index.php
@@ -16,11 +16,42 @@
 
 <body>
 
-    <header>
 
-        <img src="img/logo.png" alt="">
+    <div class="container">
+        <div class="card-login card">
 
-    </header>
+            <img src="img/logo.png" alt="Logo Plasfran">
+
+            <form>
+
+                <div class="form-group">
+                    <label class="sr-only" for="inlineFormInputGroup">Usuário</label>
+                    <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><img src="img/contato.png" alt=""></div>
+                        </div>
+                        <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Usuário">
+                    </div>
+                </div>
+
+
+
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Senha</label>
+                    <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><img src="img/cadeado.png" alt=""></div>
+                        </div>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Entrar</button>
+
+            </form>
+
+        </div>
+    </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
