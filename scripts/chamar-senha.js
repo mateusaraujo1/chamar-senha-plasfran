2a53fdb612ddc6b0919de7f8fb274e5dd80fe93d
Mateus Araújo <f.mateusaraujo20@gmail.com>
2024-07-29 11:43:15 -0300

    botoes anterior e proximo alteram a senha

--- scripts/chamar-senha.js
+++ scripts/chamar-senha.js
@@ -1,3 +1,19 @@
-function numAnterior(atual) {
-    document.getElementById('senha').innerHTML = atual - 1;
+let atual = parseInt(document.getElementById('senha').innerText);
+
+function numAnterior() 
+{
+    if (atual > 1) 
+    {
+        atual -= 1;
+        document.getElementById('senha').innerHTML = atual;
+    }
+}
+
+function numProximo() 
+{
+    if (atual < 99) 
+    {
+        atual += 1;
+        document.getElementById('senha').innerHTML = atual;
+    }
 }
