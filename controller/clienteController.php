9f07eab8e427b572cfddfb11420173173d78a976
Mateus Araújo <f.mateusaraujo20@gmail.com>
2024-08-05 11:42:21 -0300

    funcao setAll implementada

--- controller/clienteController.php
+++ controller/clienteController.php
@@ -7,8 +7,10 @@ require '../../model/fichaModel.php';
 $ficha = new Ficha();
 $connection = new Connection();
 
+$ficha->setAll(3, 4);
+
 $clienteService = new ClienteService($connection, $ficha);
 
-
+print_r($ficha);
 
 ?>
--- model/fichaModel.php
+++ model/fichaModel.php
@@ -2,8 +2,15 @@
 
     class Ficha {
         private $id;
-        private $guiche;
+        private $id_guiche_FK;
         private $senha;
+
+        public function __construct()
+        {
+            $this->senha = 0;
+            $this->id_guiche_FK = 0;
+            $this->id = 0;
+        }
 
         public function __get($atributo) 
         {
@@ -13,6 +20,16 @@
         public function __set($atributo, $value)
         {
             $this->$atributo = $value;
+        }
+
+        public function setAll($guiche, $senha) 
+        {
+            $this->senha = $senha;
+            $this->id_guiche_FK = $guiche;
+
+            $this->id = ((string)$this->id_guiche_FK) . ((string)$this->senha);
+
+            $this->id = (int)$this->id;
         }
     }
 
